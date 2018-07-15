9a76322cd1f4334ada159fbdb1503aae938e0e2f
nrcoleman <41210050+nrcoleman@users.noreply.github.com>
2018-07-14 21:20:08 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,35 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+m <- NULL
+            set <- function(y) {
+                    x <<- y
+                    soln <<- NULL
+            }
+            get <- function() x
+            setinverse <- function(s) soln<<- s
+            getinverse <- function() soln
 
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of a matrix if the result has not been
+##calculated. Otherwise, it finds the result of the calculation instead of calculating
+##the inverse again 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        soln=x$getinverse
+		if(is.null(soln)==FALSE)
+		{
+		print("Getting cached data")
+		return (soln) #Solution has already been found, so it does 
+		#not need to be calculated again.
+		}
+		else
+		{
+		#Since the inverse of the matrix has not been calculated,
+		#we must calculate the inverse
+		soln=solve(x)
+		setinverse(soln)
+		}
 }
