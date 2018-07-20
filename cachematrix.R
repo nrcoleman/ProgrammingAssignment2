985b51c6c7390c59ec3591d7c0625103b32e2be1
nrcoleman <41210050+nrcoleman@users.noreply.github.com>
2018-07-20 12:26:35 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,28 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+x=x3
+##This function makes a cached matrix. It prepares a specific matrix to be put in and resets the value of x.It sets solution to null. Once a solution has been calculated, it takes this solution and stores it in a variable x for future computations.
 
 makeCacheMatrix <- function(x = matrix()) {
-m <- NULL
+  soln<-NULL
             set <- function(y) {
                     x <<- y
                     soln <<- NULL
             }
             get <- function() x
-            setinverse <- function(s) soln<<- s
+            setinverse <- function(inv) soln<<- inv
             getinverse <- function() soln
+            setinverse(soln)
+            getinverse()
+            list(set = set, get = get,
+                 setinverse = setinverse,
+                 getinverse = getinverse)
 
 }
-
 
 ## This function calculates the inverse of a matrix if the result has not been
 ##calculated. Otherwise, it finds the result of the calculation instead of calculating
 ##the inverse again 
-
-cacheSolve <- function(x, ...) {
-        soln=x$getinverse
-		if(is.null(soln)==FALSE)
+xtemp=zeros(10)#Matrix is assumed to be invertible
+cacheSolve <- function(x, ...)
+  { 
+        #soln<-x$getinverse()
+        print("Solution reached")
+        soln
+		if(!is.null(soln))
 		{
 		print("Getting cached data")
 		return (soln) #Solution has already been found, so it does 
@@ -32,7 +37,12 @@ cacheSolve <- function(x, ...) {
 		{
 		#Since the inverse of the matrix has not been calculated,
 		#we must calculate the inverse
-		soln=solve(x)
-		setinverse(soln)
+		data<-x$get()
+		soln<-solve(data,...)
+		x$setinverse(soln)
+		return(soln)
 		}
 }
+x1=makeCacheMatrix(x)
+soln=cacheSolve(x1)
+soln
